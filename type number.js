fa7a51ecc6eb2c4688891972e19879cad6a7e349
Rud1020 <153291446+Rud1020@users.noreply.github.com>
2023-12-08 16:05:26 -0300

    Add files via upload

---
+++ type number.js
@@ -0,0 +1,21 @@
+// tipo number
+
+const MeuNumero = 3;
+
+const PrimeiroNumero1 = 1;
+const SegundoNumero2 = 2;
+
+const operaçãoMatematica = PrimeiroNumero1 - SegundoNumero2; 
+
+console.log(operaçãoMatematica);
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const PontoFlutuantesemzero = .5;
+
+const  novaOperação = PrimeiroNumero1 / numeroPontoFlutuante;
+
+console.log(novaOperação);
+
+// NaN -> Not a Number (nao é um numero)
